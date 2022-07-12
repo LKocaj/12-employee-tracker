5f371db02bffce555e8355ee9b3896f17c6cc22e
Lawrence <lawrencecaj@gmail.com>
2022-07-11 21:54:52 -0400

    sql connection

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const util = require('util');
+
+// SQL connect
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_DB'
+});
+
+connection.query = util.promisify(connection.query);
