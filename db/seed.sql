8d46a937fc7d0bacf72bc2aa2a2e0a5af6e05225
Lawrence <lawrencecaj@gmail.com>
2022-07-14 19:55:12 -0400

    fixed seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,38 +1,38 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jim', 'Bean', 1, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Johnny', 'Walker', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Henry', 'Roberts', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ellie', 'Lee', 4, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Liberty', 'Hill', 3, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ricky', 'Waters', 5, 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Helga', 'Pataki', 6, null);
+USE workDB;
 
-INSERT INTO department (department_name)
-VALUES ('Management');
-INSERT INTO department (department_name)
-VALUES ('Sales');
-INSERT INTO department (department_name)
-VALUES ('Accounting');
-INSERT INTO department (department_name)
-VALUES ('Reception');
-INSERT INTO department (department_name)
-VALUES ('Human Resources');
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('General Manager', 120000, 1);
+VALUES ("Salesman", 100000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Salesman', 80000, 2);
+VALUES ("Engineer", 150000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Accountant', 90000, 4);
+VALUES ("Financial Analyst", 120000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Receptionist', 40000, 3);
+VALUES ("Accountant", 125000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Human Resource Officer', 75000, 5);
-INSERT INTO role (title, salary, department_id)
-VALUES ('CEO', 250000, null);
+VALUES ("Lawyer", 250000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jim", "Bean", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Johnny", "Walker", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Henry", "Roberts", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ellie", "Lee", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Liberty", "Hill", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ricky", "Waters", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Helga", "Pataki", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lawrence", "Kocaj", 1, 2);
